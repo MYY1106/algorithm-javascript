b7f7dd16b8c60f515baf578410bde6db7cc2ecdf
MYY1106 <361598804@qq.com>
2022-08-04 13:32:42 +0800

    feat: 删除链表的倒数第N个结点

---
+++ 链表/19-删除链表的倒数第 N 个结点.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ * @ref https://leetcode.cn/problems/remove-nth-node-from-end-of-list/solution/dong-hua-tu-jie-leetcode-di-19-hao-wen-ti-shan-chu/
+ */
+var removeNthFromEnd = function (head, n) {
+    const newHead = new ListNode(0, head); // 虚拟头节点
+    let slow = newHead, fast = newHead;
+
+    /** @description 移动fast */
+    for (let i = 0; i < n + 1; i++)
+        fast = fast.next;
+
+    while (fast !== null) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    slow.next = slow.next.next;
+
+    return newHead.next;
+};
